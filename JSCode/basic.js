1c174e3fdc1ed5ff425688d5a7ad9742939f1380
shakedGoffer <107464725+shakedGoffer@users.noreply.github.com>
2022-10-19 14:31:02 +0300

    Add files via upload

--- JSCode/basic.js
+++ JSCode/basic.js
@@ -1,6 +1,7 @@
 
-var btnMenu = document.getElementById("button");
+var btnMenu = document.getElementsByClassName("button")[0];
 var navMenu = document.getElementById("navMenu");
+var menuContent = document.getElementById("nav-content");
 var i=0;
 btnMenu.addEventListener("mouseover", func, false);
 if (
@@ -9,8 +10,8 @@ if (
 ) 
 {
   // This checks if the current device is in fact mobile
+	menuContent.setAttribute("style", "display: none;");
 	navMenu.setAttribute("style", "min-height: 0vh; height:0%;");
-
 }
 
 function func()
@@ -23,9 +24,16 @@ function func()
 	  // This checks if the current device is in fact mobile
 	
 	if(i%2==0)
+	{
+		menuContent.setAttribute("style", "display: normal;");
 		navMenu.setAttribute("style", "min-height: 100vh; height:100%;");
+	}
 	else
+	{
+		menuContent.setAttribute("style", "display: none;");
 		navMenu.setAttribute("style", "min-height: 0vh; height:0%;");
+	}
+		
 	i++;
 	}
 }

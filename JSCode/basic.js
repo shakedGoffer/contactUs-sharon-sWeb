7cec78f6745a9c8b05d35f07130317fcefbfe3a2
shakedGoffer <107464725+shakedGoffer@users.noreply.github.com>
2022-10-20 15:51:55 +0300

    Add files via upload

--- JSCode/basic.js
+++ JSCode/basic.js
@@ -2,8 +2,11 @@
 var btnMenu = document.getElementsByClassName("button")[0];
 var navMenu = document.getElementById("navMenu");
 var menuContent = document.getElementById("nav-content");
+var bgDiv = document.getElementsByClassName("bg")[0];
 var i=0;
 btnMenu.addEventListener("mouseover", func, false);
+btnMenu.addEventListener("mouseleave", hide, false);
+
 if (
   navigator.userAgent.match(/Android/i) ||
   navigator.userAgent.match(/iPhone/i)
@@ -12,6 +15,7 @@ if (
   // This checks if the current device is in fact mobile
 	menuContent.setAttribute("style", "display: none;");
 	navMenu.setAttribute("style", "min-height: 0vh; height:0%;");
+	bgDiv.setAttribute("style", "visibility: hidden;");
 }
 
 function func()
@@ -27,13 +31,26 @@ function func()
 	{
 		menuContent.setAttribute("style", "display: normal;");
 		navMenu.setAttribute("style", "min-height: 100vh; height:100%;");
+		bgDiv.setAttribute("style", "visibility: visible;");
 	}
 	else
 	{
 		menuContent.setAttribute("style", "display: none;");
 		navMenu.setAttribute("style", "min-height: 0vh; height:0%;");
+		bgDiv.setAttribute("style", "visibility: hidden;");
 	}
 		
+	i++;
+	}
+}
+
+function hide()
+{  
+	if (
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/iPhone/i) ) 
+	{
+	  // This checks if the current device is in fact mobile
 	i++;
 	}
 }
